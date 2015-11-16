7bd191c0c6294979998c5ca80dcca8bef50466ba
codingJWilliams <course@jaywilliams.me>
2015-11-16 19:58:25 +0000

    Updated main.py to make it 95 lines long
    Used for loop for numChecker function to compact greatly

--- Lottery.py
+++ Lottery.py
@@ -1,41 +1,22 @@
 import random
 import time
-#version 2
+#version 2 compacted
 
 correct = ['x','x','x','x','x','x','x']
 numCorrect = 0
 stopProgram = False
 numsint = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-# Make a custom function for checking nums as i may use more than once
 def numChecker(check_string):
     right = 0
-    if check_string[0] in numsint:
-        right += 1
-    if check_string[1] in numsint:
-        right += 1
-    if check_string[2] in numsint:
-        right = right + 1
-    if check_string[3] in numsint:
-        right = right + 1
-    if check_string[4] in numsint:
-        right = right + 1
-    if check_string[5] in numsint:
-        right = right + 1
-    if check_string[6] in numsint:
-        right = right + 1
+    for i in range(7):
+        if check_string[i] in numsint:
+            right += 1
     if right == 7:
         return True
     else:
         return False
-
-
-# Define variables
 chosen = []
-stopMsg = "YOU HAVE TRIED TO CHEAT THE LOTTERY SYSTEM. WOE BETIDE YE"
-# input
 userNumbers = input(
     '                             National Lotto! \n   -Please enter your numbers to have a chance of winning the' +
     'JACKPOT!\n\n   -Please enter 7 numbers to be in with a chance to win 1,000,000!\n>')
@@ -46,53 +27,49 @@ try:
 except:
     numCorrect = 1
 if numCorrect == 0:
-    print(stopMsg)
+    print("YOU HAVE TRIED TO CHEAT THE LOTTERY SYSTEM. WOE BETIDE YE")
     stopProgram = True
 else:
 
     allNumbers = numChecker(userNumbers)
     if not allNumbers:
-        print(stopMsg)
+        print("YOU HAVE TRIED TO CHEAT THE LOTTERY SYSTEM. WOE BETIDE YE")
         stopProgram = True
 
 if not stopProgram:
     # Code for the lotto system.
     numCorrect = 0
-    char1 = random.choice(nums)
-    char2 = random.choice(nums)
-    char3 = random.choice(nums)
-    char4 = random.choice(nums)
-    char5 = random.choice(nums)
-    char6 = random.choice(nums)
-    char7 = random.choice(nums)
-    altogether = str(char1) + str(char2) + str(char3) + str(char4) + str(char5) + str(char6) + str(char7)
-    if altogether[0] == userNumbers[0]:
+    chars = []
+    for i in range(7):
+        chars.append(random.choice(numsint))
+    compnum = sum(chars)
+    if compnum[0] == userNumbers[0]:
         numCorrect += 1
-        num1 = userNumbers[0]
+        correct[0] = userNumbers[0]
 
-    if altogether[1] == userNumbers[1]:
+    if compnum[1] == userNumbers[1]:
         numCorrect += 1
-        num2 = userNumbers[1]
+        correct[1] = userNumbers[1]
 
-    if altogether[2] == userNumbers[2]:
+    if compnum[2] == userNumbers[2]:
         numCorrect += 1
-        num3 = userNumbers[2]
+        correct[2] = userNumbers[2]
 
-    if altogether[3] == userNumbers[3]:
+    if compnum[3] == userNumbers[3]:
         numCorrect += 1
-        num4 = userNumbers[3]
+        correct[3] = userNumbers[3]
 
-    if altogether[4] == userNumbers[4]:
+    if compnum[4] == userNumbers[4]:
         numCorrect += 1
-        num5 = userNumbers[4]
+        correct[4] = userNumbers[4]
 
-    if altogether[5] == userNumbers[5]:
+    if compnum[5] == userNumbers[5]:
         numCorrect += 1
-        num6 = userNumbers[5]
-    if altogether[6] == userNumbers[6]:
+        correct[5] = userNumbers[5]
+        
+    if compnum[6] == userNumbers[6]:
         numCorrect += 1
-        num7 = userNumbers[6]
-
+        correct[6] = userNumbers[6]
     if numCorrect == 1:
         prize = "0 :("
     elif numCorrect == 2:
@@ -109,11 +86,10 @@ if not stopProgram:
         prize = "a jackpot of 1,000,000"
     else:
         prize = "nothing, sorry :-( "
-    winnings = num1 + num2 + num3 + num4 + num5 + num6 + num7
     print("You won:")
     time.sleep(0.5)
     print("*drum-roll*")
     time.sleep(1.5)
-    print("You've won {0}, with {1} numbers.\nThe numbers that matched were:\n~{2}".format(prize, numCorrect, winnings))
+    print("You've won {0}, with {1} numbers.\nThe numbers that matched were:\n~{2}".format(prize, numCorrect, winnings = correct[0] + correct[1] + correct[2] + correct[3] + correct[4] + correct[5] + correct[6]))
 elif stopProgram:
     exit()
